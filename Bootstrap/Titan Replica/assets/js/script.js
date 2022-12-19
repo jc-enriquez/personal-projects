1911ca0f4f07437fbe4fa599a005d78c74a9343a
unknown <john.christopher.enriquez2@gmail.com>
2022-12-19 15:55:34 +0800

    fourth commit

--- Bootstrap/Titan Replica/assets/js/script.js
+++ Bootstrap/Titan Replica/assets/js/script.js
@@ -1,3 +1,35 @@
-var swiper = new swiper(".swiper", {
+var swiper = new Swiper(".promo-swiper", {
   direction: "vertical",
+  autoplay: { delay: 3000 },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  height: 30,
+});
+
+var sectionSwiper = new Swiper(".section-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
